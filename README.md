74280f922c50fc11eada3bf48f18a3b14e0c2a60
Tvang93 <vangt39@gmail.com>
2025-01-17 10:17:45 -0800

    initial commit, made home page, working on pve page

---
+++ README.md
@@ -0,0 +1,45 @@
+### Thao Vang
+
+### 1/17/25
+
+### Assignment: Rock Paper Scissor Lizard Spock
+
+### Brief Description:
+    Here are the requirements:
+        1v1 and 1vCPU
+        Prototype for this work or a detailed flow chart to illustrate the logic or both!
+        Different modes: 1 win / 3 out of 5 / 4 out of 7
+        Single Page or Multiple page Application for Desktop
+        Must have a way to show the rules of the game
+        Must be designed, on Github, and Hosted on Vercel
+
+    Game Rules:
+        Scissors cuts paper.
+        Scissors decapitates lizard.
+        Rock crushes lizard.
+        Rock crushes scissors.
+        Paper covers rock.
+        Paper disproves Spock.
+        Lizard poisons Spock.
+        Lizard eats paper.
+        Spock smashes scissors.
+        Spock vaporizes rock.
+
+    On CPU mode - use any one the following random APIcalls : 
+        Use your created API call to generate the CPU response 
+
+### API Link: 
+https://rckpprscrlzdspk-gnb2bqcpf6adgrdq.westus-01.azurewebsites.net/RckPprScrLzdSpk/CpuChoice
+
+### Figma:
+Appoved by Jacob
+https://www.figma.com/design/ZnIXW7lhkrCUHutajkpXOG/Projects-2?node-id=84-2&t=AlQ1qNp5Jt9tu96G-1
+
+### Peer Review:
+by: 
+
+
+
+
+
+
---
+++ Scripts/app.js
@@ -0,0 +1,63 @@
+const homeBtnContainer = document.getElementById("homeBtnContainer");
+const homePlayBtn = document.getElementById("homePlayBtn");
+
+homePlayBtn.addEventListener("click", function(){
+    homePlayBtn.classList = "inactive";
+    let chosePvp = false;
+    let chosePve = false;
+
+    let pvpBtn = document.createElement("button");
+    pvpBtn.innerText = "vs Player";
+
+    let pveBtn = document.createElement("button");
+    pveBtn.innerText = "vs Comp";
+
+    let oneMatchBtn = document.createElement("button");
+    oneMatchBtn.innerText = "Sudden Death"
+
+    let fiveMatchBtn = document.createElement("button");
+    fiveMatchBtn.innerText = "Best of 5"
+
+    let sevenMatchBtn = document.createElement("button");
+    sevenMatchBtn.innerText = "Best of 7"
+
+    let backBtn = document.createElement("button");
+    backBtn.innerText = "Back";
+    backBtn.style.background = "red"
+
+    pveBtn.addEventListener("click", function(){
+        chosePve = true;
+        pvpBtn.classList = "inactive";
+        pveBtn.classList = "inactive";
+        homeBtnContainer.appendChild(oneMatchBtn);
+        homeBtnContainer.appendChild(fiveMatchBtn);
+        homeBtnContainer.appendChild(sevenMatchBtn);
+        homeBtnContainer.appendChild(backBtn);
+    });
+
+    backBtn.addEventListener("click", function(){
+        if(chosePve){
+            chosePve = false;
+            pvpBtn.classList = "";
+            pveBtn.classList = "";
+            oneMatchBtn.remove();
+            fiveMatchBtn.remove();
+            sevenMatchBtn.remove();
+        }else{
+            pvpBtn.remove();
+            pveBtn.remove();
+            backBtn.remove();
+            homePlayBtn.classList = "";
+        }
+    });
+
+    oneMatchBtn.addEventListener("click", function(){
+        const link = document.createElement("a");
+        link.href = './pages/playerVsComp.html';
+        link.click();
+    });
+
+    homeBtnContainer.appendChild(pvpBtn);
+    homeBtnContainer.appendChild(pveBtn);
+    homeBtnContainer.appendChild(backBtn);
+});
